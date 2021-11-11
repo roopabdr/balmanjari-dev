740fadd21865fb607e5db0afb9d2f2f71aef5d60
Roopa Madihally <roopa.madihally@pwc.com>
2021-11-11 12:54:28 +0530

    added logos and new deployment of new homepage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import './App.scss';
+import BalmanjariLogo from './Logo/Balmanjari_logo-2.png';
+// import BalmanjariLogo from './Logo/Balmanjari logo.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <div className="om">&#2384;</div> */}
+      <div className="banner">
+        <div className="balmanjariLogo">
+          <img src={BalmanjariLogo} alt="Balmanjari" />
+          <span>Balmanjari</span>
+        </div>
+        <div>
+        <span className="heading" data-shadow='Sanskar Science &amp; Shlokas'>Sanskar Science &amp; Shlokas</span>
+        </div>
+      </div>
+      <div className="homeContent">
+        The concept of Balmanjari is Sanskar Science and Shlokas. It's an effort to teach children what is Sanatana Dharma is and that it's not a religion but science.
+      </div>
     </div>
   );
 }
