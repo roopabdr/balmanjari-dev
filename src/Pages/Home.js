2e84f773a2e9e401e8fa830bd7f788332d420a49
Roopa Madihally <roopa.madihally@pwc.com>
2021-11-12 18:02:29 +0530

    added kid classes as cards

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,6 +1,10 @@
 import BalmanjariLogo from '../Logo/Balmanjari_logo-2.png';
 // import BalmanjariLogo from '../Logo/Balmanjari logo.png';
 import '../App.scss';
+import '../Components.css';
+import BhagavadGita from '../Images/BhagavadGita1.png';
+import Shloka from '../Images/Shloka2.png';
+import ReligiousBeliefs from '../Images/ReligiousBeliefs3.png';
 
 function Home() {
     return (
@@ -15,8 +19,51 @@ function Home() {
                 </div>
             </div>
             <div className="homeContent">
-                The concept of Balmanjari is Sanskar Science and Shlokas. It's an effort to teach children what is Sanatana Dharma is and that it's not a religion but science.
+                <p>
+                    The concept of Balmanjari is Sanskar Science and Shlokas. It's an effort to teach children what is Sanatana Dharma is and that it's not a religion but science.
+                </p>
+                <div className="cards">
+                    <div className="flip-card">
+                        <div className="flip-card-inner">
+                            <div className="flip-card-front">
+                                <img src={BhagavadGita} alt="Bhagavad Gita" />
+                                <h4><b>Bhagavad Gita</b></h4>
+                            </div>
+                            <div className="flip-card-back">
+                                <h2>Bhagavad Gita</h2>
+                                <p>Learn to recite Bhagavad Gita and their meaning</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="flip-card">
+                        <div className="flip-card-inner">
+                            <div className="flip-card-front">
+                                <img src={Shloka} alt="Shlokas" />
+                                <h4><b>Shlokas</b></h4>
+                            </div>
+                            <div className="flip-card-back">
+                                <h2>Shlokas</h2>
+                                <p>Learn to recite Karagre Shloka, Navagraha Stotra and many more and learn their meaning while doing so</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="flip-card">
+                        <div className="flip-card-inner">
+                            <div className="flip-card-front">
+                                <img src={ReligiousBeliefs} alt="Rituals and Religious Beliefs" />
+                                <h4><b>Rituals</b></h4>
+                            </div>
+                            <div className="flip-card-back">
+                                <h2>Rituals</h2>
+                                <p>Learn to perform rituals and understand why they are done</p>
+                            </div>
+                        </div>
+                    </div>                                                         
+                </div>
             </div>
+            {/* Below are Kids Classes details */}
         </div>
     );
 }
